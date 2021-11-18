9d7fe5c01a3930c83c2add2258b128e1f853ba43
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-11-18 09:42:27 +0000

    Created the pages required within the application and added imports ready for the bottom navigation bar

--- MentalWellnessApplication/src/components/App.js
+++ MentalWellnessApplication/src/components/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 
+import 'react-native-gesture-handler';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+// Importing the required third party modules to create the navigation bar.
+import { NavigationContainer} from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// Importing all of the different components for each different page.
+
 export default class App extends Component {
     render() {
         return (
