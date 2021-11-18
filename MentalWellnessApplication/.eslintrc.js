89706b88c79881b6e795c06efabf48b6594a231d
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-11-18 18:44:29 +0000

    Added eslint rules and comments to ensure code quality. Updated navbar to include custome styled home button that is set as default.

--- MentalWellnessApplication/.eslintrc.js
+++ MentalWellnessApplication/.eslintrc.js
@@ -1,4 +1,29 @@
+// ESLINT FILE FROM: https://github.com/jscomplete/advanced-react/blob/master/.eslintrc.js
+// This eslint file was selected as it uses the babel eslint with the recommended eslint and react rules. 
+
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  'parser': 'babel-eslint',
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jest': true,
+  },
+  'extends': ['eslint:recommended', 'plugin:react/recommended'],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true
+    },
+    'sourceType': 'module'
+  },
+  'plugins': [ 'react' ],
+  'rules': {
+    'react/prop-types': ['off'],
+    'linebreak-style': ['error','unix'],
+    'quotes': ['error','single'],
+    'semi': ['error','always'],
+    'no-console': ['warn', { 'allow': ['info', 'error'] }]
+  }
 };
--- MentalWellnessApplication/src/components/App.js
+++ MentalWellnessApplication/src/components/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
 
 import 'react-native-gesture-handler';
-
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // Importing the required third party modules to create the navigation bar.
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Importing all of the different components for each different page.
@@ -16,33 +15,40 @@ import EntriesList from './pages/EntriesList';
 import MoodJournal from './pages/MoodJournal';
 import HRMonitoring from './pages/HRMonitoring';
 
+// Variable that is useed to create all of the tabs required for the navigation bar.
 const Tab = createBottomTabNavigator();
 
 export default class App extends Component {
     render() {
         return (
             <View style={styles.mainContainer}>
+              {/* The Navigation bar creation tag that sets the default tab to 'home', adds tint styling for when an
+                option is selected and returns the user to the home tab if back is pressed from any tab other than home,
+                where the application will close. 
+
+                * REFERENCE ACCESSED 18/11/2021 https://aboutreact.com/react-native-bottom-navigation/
+                * Used to learn how to create a navigation bar.
+                */}
                 <NavigationContainer>
                     <Tab.Navigator
-                        initialRouteName='Feed'
-                        screenOptions={{"tabBarActiveTintColor": "#42f44b",
-                        "tabBarStyle": [
+                        initialRouteName='Home'
+                        backBehavior='initialRoute'
+                        screenOptions={{
+                        'tabBarStyle': [
                           {
-                            "display": "flex"
+                            'display': 'flex',
+                            'tabBarActiveTintColor': '#448aff',
                           },
-                          null
-                        ]}} >
+                          null ]}} >
 
                         <Tab.Screen
-                            name='View all Entries' // to remove the top nav bar delete this.
-                            component={EntriesList}
+                            name='View all Entries' // Adds the page heading to the top of the screen. Can be removed by removing line.
+                            component={EntriesList} // The component to switch to when tab is pressed.
                             options={{
                                 tabBarLabel: 'View all Entries',
                                 tabBarIcon: ({ color, size }) => (
-                                    <Icon name='book' color={color} size={size} />
-                                ),
-                            }} />
-
+                                    <Icon name='book' color={color} size={size} /> // Sets the icon to the default color and size.
+                                )}} />
                         <Tab.Screen
                             name='Add an Entry'
                             component={AddNewEntry}
@@ -50,19 +56,18 @@ export default class App extends Component {
                                 tabBarLabel: 'Add an Entry',
                                 tabBarIcon: ({ color, size }) => (
                                     <Icon name='plus-circle' color={color} size={size} />
-                                ),
-                            }} />
-
+                                )}} />
                         <Tab.Screen
                             name='Home' 
                             component={Home}
                             options={{
                                 tabBarLabel: 'Home',
                                 tabBarIcon: ({ color, size }) => (
-                                    <Icon name='home' color={color} size={size} />
-                                ),
-                            }} />
-
+                                    {/* Adds custom styling for center button to be bigger and circular. */},
+                                    <View style={styles.centerNavBTN}>  
+                                        <Icon name='home' color={color} size={size} />
+                                    </View>
+                                )}} />
                         <Tab.Screen
                             name='View Moods'
                             component={MoodJournal}
@@ -70,9 +75,7 @@ export default class App extends Component {
                                 tabBarLabel: 'View Moods',
                                 tabBarIcon: ({ color, size }) => (
                                     <Icon name='calendar-multiselect' color={color} size={size} />
-                                ),
-                            }} />
-
+                                )}} />
                         <Tab.Screen
                             name='Heart Rate Monitoring'
                             component={HRMonitoring}
@@ -80,14 +83,12 @@ export default class App extends Component {
                                 tabBarLabel: 'HR Monitoring',
                                 tabBarIcon: ({ color, size }) => (
                                     <Icon name='heart-pulse' color={color} size={size} />
-                                ),
-                            }} />
+                                )}} />
 
                     </Tab.Navigator>
-
                 </NavigationContainer>
             </View>
-        )
+        );
     }
 }
 
@@ -96,4 +97,14 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
     },
-})
+    centerNavBTN: {
+        position: 'absolute',
+        bottom: 5,
+        height: 58,
+        width: 58,
+        borderRadius: 58,
+        backgroundColor: '#00e676',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
