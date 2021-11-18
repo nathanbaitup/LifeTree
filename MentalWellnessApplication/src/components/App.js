2b6cb097fffa8f3e5c96fe65fb3a7e967b8a97eb
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-11-18 15:55:33 +0000

    Modified navigation bar to be in the correct order and added support to access react-native-icons in the application.

--- MentalWellnessApplication/src/components/App.js
+++ MentalWellnessApplication/src/components/App.js
@@ -3,10 +3,10 @@ import { Text, View, StyleSheet } from 'react-native'
 
 import 'react-native-gesture-handler';
 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // Importing the required third party modules to create the navigation bar.
-import { NavigationContainer} from '@react-navigation/native'
+import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // Importing all of the different components for each different page.
@@ -21,61 +21,67 @@ const Tab = createBottomTabNavigator();
 export default class App extends Component {
     render() {
         return (
-            <View style = {styles.mainContainer}>
+            <View style={styles.mainContainer}>
                 <NavigationContainer>
                     <Tab.Navigator
                         initialRouteName='Feed'
-                        tabBarOptions={{ activeTintColor: '#42f44b' }} >
+                        screenOptions={{"tabBarActiveTintColor": "#42f44b",
+                        "tabBarStyle": [
+                          {
+                            "display": "flex"
+                          },
+                          null
+                        ]}} >
 
                         <Tab.Screen
-                            name='Home' // to remove the top nav bar delete this.
+                            name='View all Entries' // to remove the top nav bar delete this.
+                            component={EntriesList}
+                            options={{
+                                tabBarLabel: 'View all Entries',
+                                tabBarIcon: ({ color, size }) => (
+                                    <Icon name='book' color={color} size={size} />
+                                ),
+                            }} />
+
+                        <Tab.Screen
+                            name='Add an Entry'
+                            component={AddNewEntry}
+                            options={{
+                                tabBarLabel: 'Add an Entry',
+                                tabBarIcon: ({ color, size }) => (
+                                    <Icon name='plus-circle' color={color} size={size} />
+                                ),
+                            }} />
+
+                        <Tab.Screen
+                            name='Home' 
                             component={Home}
                             options={{
                                 tabBarLabel: 'Home',
                                 tabBarIcon: ({ color, size }) => (
-                                <MaterialCommunityIcons name='home' color={color} size={size} />
+                                    <Icon name='home' color={color} size={size} />
                                 ),
                             }} />
-                        <Tab.Screen 
-                            name='Add an Entry'
-                            component={AddNewEntry}
-                            options={{ 
-                                tabBarLabel: 'Add an Entry',
-                                tabBarIcon: ({ color, size }) => (
-                                    <MaterialCommunityIcons name='add' color={color} size={size} />
-                                ),
-                            }}
-                            />
-                            <Tab.Screen 
-                            name='View all Entries'
-                            component={EntriesList}
-                            options={{ 
-                                tabBarLabel: 'View all Entries',
-                                tabBarIcon: ({ color, size }) => (
-                                    <MaterialCommunityIcons name='add' color={color} size={size} />
-                                ),
-                            }}
-                            />
-                            <Tab.Screen 
+
+                        <Tab.Screen
                             name='View Moods'
                             component={MoodJournal}
-                            options={{ 
+                            options={{
                                 tabBarLabel: 'View Moods',
                                 tabBarIcon: ({ color, size }) => (
-                                    <MaterialCommunityIcons name='add' color={color} size={size} />
+                                    <Icon name='calendar-multiselect' color={color} size={size} />
                                 ),
-                            }}
-                            />
-                             <Tab.Screen 
+                            }} />
+
+                        <Tab.Screen
                             name='Heart Rate Monitoring'
                             component={HRMonitoring}
-                            options={{ 
+                            options={{
                                 tabBarLabel: 'HR Monitoring',
                                 tabBarIcon: ({ color, size }) => (
-                                    <MaterialCommunityIcons name='add' color={color} size={size} />
+                                    <Icon name='heart-pulse' color={color} size={size} />
                                 ),
-                            }}
-                            />
+                            }} />
 
                     </Tab.Navigator>
 
@@ -87,7 +93,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     mainContainer: {
-        flex:1,
+        flex: 1,
         justifyContent: 'center',
     },
 })
