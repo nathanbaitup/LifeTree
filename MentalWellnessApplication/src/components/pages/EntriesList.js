c311cae4e54c8ba77a030c1f9d829ef4ab3121f8
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-12-07 16:25:27 +0000

    Replaced SectionList with FlatList as it is more useful with the intended purposes of the List View. Replaced the filter button with a searchbar, allowing the user to filter the list by Date or by the mood they entered. Working on styling of the journal list.

--- MentalWellnessApplication/src/components/pages/EntriesList.js
+++ MentalWellnessApplication/src/components/pages/EntriesList.js
@@ -1,47 +1,80 @@
 import React, { Component } from 'react';
-import { Text, View, ImageBackground, ScrollView, Dimensions, TouchableOpacity, SectionList, StyleSheet } from 'react-native';
-
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
-import { Dropdown } from 'react-native-material-dropdown';
-
-import { Picker } from '@react-native-picker/picker';
-
-import { FloatingAction } from "react-native-floating-action";
-
-import ActionButton from 'react-native-action-button';
-
+import { Text, View, ImageBackground, Dimensions, FlatList, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
 export default class EntriesList extends Component {
+    constructor(props) {
+        super(props);
 
-    //pickerRef = useRef();
+        this.state = {
+            //currently used as test data, will be removed and replaced with data taken from database.
+            allEntries: [],
+            searchText: '',
+            filteredData: [],
+        };
 
-    // filterDropdown = () => {
-    //     pickerRef.current.focus();
-    // }
+        this.arrayHolder = [];
+    }
+
+    componentDidMount() {
+        let allEntries = [
+            { key: '0', date: 'Jan', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam pulvinar sollicitudin hendrerit. Donec a dolor lacus. Fusce feugiat velit vitae odio pretium ultricies. Proin sollicitudin nulla in ornare ullamcorper. In ut eros eget nisl placerat placerat sit amet sit amet nibh. Morbi porta fringilla metus, quis tincidunt augue lacinia ac. Quisque ornare velit imperdiet dictum fringilla. Donec in turpis ligula. Vestibulum et porta leo. Nulla molestie elit quam, in ornare neque malesuada ac. Curabitur aliquam in massa quis mattis. Aliquam sit amet est id ipsum pretium eleifend a id sem. Nam congue nisl ipsum, id ullamcorper eros elementum sed. ', mood: 'happy' },
+            { key: '1', date: 'Jan', description: 'blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah', mood: 'happy' },
+            { key: '2', date: 'Jan', description: 'blah blah', mood: 'happy' },
+            { key: '3', date: 'Jun', description: 'blah blah', mood: 'meh' },
+            { key: '4', date: 'Aug', description: 'blah blah', mood: 'happy' },
+            { key: '5', date: 'Aug', description: 'blah blah', mood: 'angry' },
+            { key: '6', date: 'Sep', description: 'blah blah', mood: 'happy' },
+            { key: '7', date: 'May', description: 'blah blah', mood: 'meh' },
+            { key: '8', date: 'Jul', description: 'blah blah', mood: 'happy' },
+            { key: '9', date: 'Mar', description: 'blah blah', mood: 'sad' },
+        ];
+        this.setState({ allEntries: allEntries });
+    }
+
+
+    // REFERENCE ACCESSED 07/12/2021 https://stackoverflow.com/a/55949691
+    // Used to allow the user to search for journal entires based on the date for easier viewing.
+    search = (searchText) => {
+        this.setState({ searchText: searchText });
+        let filteredData = this.state.allEntries.filter(function (item) {
+            return item.date.includes(searchText) || item.mood.includes(searchText);
+        });
+
+        this.setState({ filteredData: filteredData });
+    };
+    // END REFERENCE
+
+
 
     render() {
         return (
             <ImageBackground source={require('../resources/img/background.png')} style={{ width: '100%', height: '100%', opacity: 50 }} >
 
                 <View style={styles.mainContainer}>
-                        <View style={styles.filterBar}>
-                            <Text>Search by....</Text>
-                            <TouchableOpacity onPress={this.filterDropdown}>
-                                <Icon name='filter-variant' color={'#000000'} size={35} style={styles.filter} />
-                            </TouchableOpacity>
-                        </View>
                     
-                    <View style={styles.contentContainer}>
-                        <SectionList
-                            sections={[
-                                { title: 'Today', data: ['blah blah 1', 'blah blah 2', 'blah blah 3'] },
-                                { title: 'This Week', data: ['blah blah 4', 'blah blah 5'] },
-                                { title: 'This Month', data: ['blah blah 6', 'blah blah 7', 'blah blah 8', 'blah blah 9', 'blah blah 10', 'blah blah 11', 'blah blah 12', 'blah blah 13', 'blah blah 14', 'blah blah 15', 'blah blah 16', 'blah blah 17', 'blah blah 18', 'blah blah 19'] },
+                    <SearchBar round={true}
+                        lightTheme={true}
+                        placeholder='Search by Date or Mood...'
+                        autoCapitalize='none'
+                        onChangeText={this.search}
+                        value={this.state.searchText} />
 
-                            ]} renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
-                            renderSectionHeader={({ section }) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-                            keyExtractor={(item, index) => index} />
+                    <View style={styles.contentContainer}>
+                        <FlatList
+                            // REFERENCE ACCESSED 07/12/2021 https://stackoverflow.com/a/55949691
+                            // Used to allow the user to search for journal entires based on the date for easier viewing.
+                            data={this.state.filteredData && this.state.filteredData.length > 0 ? this.state.filteredData : this.state.allEntries}
+                            keyExtractor={(item) => `item-${item.key}`}
+                            // END REFERENCE
+                            renderItem={({ item }) => <View style={styles.listView}>
+                                <Text style={styles.entryDate}>{item.date}</Text>
+                                <Text style={styles.entryDesc} numberOfLines = {2} >{item.description}</Text>
+                                <Text style={styles.entryMood}> What</Text>
+                            </View>}
+
+                        />
+
                     </View>
                 </View>
             </ImageBackground>
@@ -69,22 +102,27 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: '#FFFFFF',
         borderRadius: 25,
-        height: height/1.45,
-        
+        height: height / 1.45,
     },
-    sectionHeader: {
-        paddingTop: 2,
+    listView: {
+        borderBottomWidth: 1,
+        borderRadius: 0.5,
+        borderColor: 'rgba(215, 210, 210, 0.4)',
+    },
+    entryDesc: {
+        paddingTop: 10,
         paddingLeft: 10,
-        paddingRight: 10,
-        paddingBottom: 2,
         fontSize: 14,
-        fontWeight: 'bold',
-        backgroundColor: '#e0e0de',
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
         height: 44,
         color: '#000000',
+        
+    },
+    entryDate: {
+        paddingTop: 8,
+        paddingLeft: 10,
+        paddingRight: 10,
+        fontSize: 18,
+        color: '#000000',
+        fontWeight: 'bold',
     },
 });
