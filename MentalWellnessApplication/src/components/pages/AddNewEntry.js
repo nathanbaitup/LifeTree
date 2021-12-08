21f8a4f64b2dcac24f9e2077a6418dd027467de9
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-12-08 17:52:52 +0000

    Added date to be shown to the user and also to the state ready to be stored for backend. Added mood to be stored in the state, will be updated through onPress of button when backend is done.

--- MentalWellnessApplication/src/components/pages/AddNewEntry.js
+++ MentalWellnessApplication/src/components/pages/AddNewEntry.js
@@ -7,12 +7,15 @@ export default class AddNewEntry extends Component {
         isSadSelected: false,
         isMehSelected: false,
         isHappySelected: false,
+
         journalEntry: '',
+        dateOfEntry: null,
+        moodForEntry: '',
     };
 
     // Used for onPress to set the state to angry and deselect all other options.
     isAngry = () => {
-        if (!this.state.isAngrySelected){
+        if (!this.state.isAngrySelected) {
             this.setState({ isAngrySelected: true });
             this.setState({ isSadSelected: false });
             this.setState({ isMehSelected: false });
@@ -21,7 +24,7 @@ export default class AddNewEntry extends Component {
     };
     // Used for onPress to set the state to sad and deselect all other options.
     isSad = () => {
-        if (!this.state.isSadSelected){
+        if (!this.state.isSadSelected) {
             this.setState({ isAngrySelected: false });
             this.setState({ isSadSelected: true });
             this.setState({ isMehSelected: false });
@@ -30,7 +33,7 @@ export default class AddNewEntry extends Component {
     };
     // Used for onPress to set the state to meh and deselect all other options.
     isMeh = () => {
-        if (!this.state.isMehSelected){
+        if (!this.state.isMehSelected) {
             this.setState({ isAngrySelected: false });
             this.setState({ isSadSelected: false });
             this.setState({ isMehSelected: true });
@@ -39,7 +42,7 @@ export default class AddNewEntry extends Component {
     };
     // Used for onPress to set the state to happy and deselect all other options.
     isHappy = () => {
-        if (!this.state.isHappySelected){
+        if (!this.state.isHappySelected) {
             this.setState({ isAngrySelected: false });
             this.setState({ isSadSelected: false });
             this.setState({ isMehSelected: false });
@@ -48,56 +51,80 @@ export default class AddNewEntry extends Component {
     };
 
 
+    componentDidMount() {
+        const date = new Date();
+        var options = {day: 'numeric', month: 'long', year: 'numeric'};
+        const dateOfEntry = date.toLocaleDateString('en-US', options);
+        this.setState({dateOfEntry: dateOfEntry});  
+    }
+
+    updateStateMood = () => {
+        if (this.state.isAngrySelected){
+            this.setState({moodForEntry: 'angry'});
+        } else if (this.state.isSadSelected) {
+            this.setState({moodForEntry: 'sad'});
+        } else if (this.state.isMehSelected){
+            this.setState({moodForEntry: 'meh'});
+        } else if (this.state.isHappySelected){
+            this.setState({moodForEntry: 'happy'});
+        } else {
+            this.setState({moodForEntry: 'no mood selected'});
+        }
+    }
+
+
     // TODO: ADD DATE TO BE STORED TO THE STATE.
     render() {
         return (
-            <ImageBackground source={require('../resources/img/background.png')} style = {{width:'100%', height:'100%', opacity:50}} >
-            <View style={styles.mainContainer}>
-                <View style={styles.contentContainer}>
-                    <Text style={styles.header}>How are you feeling today? </Text>
-                    <View style={styles.moodModules}>
-                    <TouchableOpacity style={this.state.isAngrySelected ? styles.moodModSelected :styles.moodModUnselected} onPress={this.isAngry} >
-                        <Image source={require('../resources/img/faces/angry.png')} style={styles.moodFaces} />
-                            <Text>Angry</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={this.state.isSadSelected ? styles.moodModSelected :styles.moodModUnselected} onPress={this.isSad}>
-                        <Image source={require('../resources/img/faces/sad.png')} style={styles.moodFaces} />
-                            <Text>Sad</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={this.state.isMehSelected ? styles.moodModSelected :styles.moodModUnselected} onPress={this.isMeh}>
-                        <Image source={require('../resources/img/faces/meh.png')} style={styles.moodFaces} />
-                            <Text>Meh</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={this.state.isHappySelected ? styles.moodModSelected :styles.moodModUnselected} onPress={this.isHappy}>
-                        <Image source={require('../resources/img/faces/happy.png')} style={styles.moodFaces} />
-                            <Text>Happy</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <View>
-                        <Text style={styles.journalHeader}>Any comments for the day? </Text>
-                        <TextInput style={styles.journalEntry}
-                            placeholder='Feel free to dump as much or as little information in here as you want. We wont judge you at all. We promise! '
-                            numberOfLines={10}
-                            multiline = {true}
-                            onChangeText={(journalEntry) => {this.setState({journalEntry:journalEntry});}}
-                            value={this.state.journalEntry}
-                        />
-                        <View style={styles.submitButtonContainer}>
-                            {/* TODO: MAKE ONPRESS TO SAVE DATA WHEN BACKEND HAS BEEN CONNECTED */}
-                            <TouchableOpacity style={styles.submitButton} onPress={console.log(this.state.journalEntry)} >
-                                <Text style={styles.submitText}>Submit</Text>
+            <ImageBackground source={require('../resources/img/background.png')} style={{ width: '100%', height: '100%', opacity: 50 }} >
+                <View style={styles.mainContainer}>
+                    <View style={styles.contentContainer}>
+                        <Text style={styles.header}>How are you feeling today? </Text>
+                        <View style={styles.moodModules}>
+                            <TouchableOpacity style={this.state.isAngrySelected ? styles.moodModSelected : styles.moodModUnselected} onPress={this.isAngry} >
+                                <Image source={require('../resources/img/faces/angry.png')} style={styles.moodFaces} />
+                                <Text>Angry</Text>
                             </TouchableOpacity>
+                            <TouchableOpacity style={this.state.isSadSelected ? styles.moodModSelected : styles.moodModUnselected} onPress={this.isSad}>
+                                <Image source={require('../resources/img/faces/sad.png')} style={styles.moodFaces} />
+                                <Text>Sad</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={this.state.isMehSelected ? styles.moodModSelected : styles.moodModUnselected} onPress={this.isMeh}>
+                                <Image source={require('../resources/img/faces/meh.png')} style={styles.moodFaces} />
+                                <Text>Meh</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={this.state.isHappySelected ? styles.moodModSelected : styles.moodModUnselected} onPress={this.isHappy}>
+                                <Image source={require('../resources/img/faces/happy.png')} style={styles.moodFaces} />
+                                <Text>Happy</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View>
+                            <Text style={styles.journalHeader}>Any comments for the day? </Text>
+                            <TextInput style={styles.journalEntry}
+                                placeholder='Feel free to dump as much or as little information in here as you want. We wont judge you at all. We promise! '
+                                numberOfLines={10}
+                                multiline={true}
+                                onChangeText={(journalEntry) => { this.setState({ journalEntry: journalEntry }); }}
+                                value={this.state.journalEntry}
+                            />
+
+                            <Text style={styles.date}>Todays Date: {this.state.dateOfEntry} </Text>
+                            <View style={styles.submitButtonContainer}>
+                                {/* TODO: MAKE ONPRESS TO SAVE DATA WHEN BACKEND HAS BEEN CONNECTED */}
+                                <TouchableOpacity style={styles.submitButton} onPress={console.log(this.state.journalEntry)} >
+                                    <Text style={styles.submitText}>Submit</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
                     </View>
                 </View>
-            </View>
             </ImageBackground>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    mainContainer:{
+    mainContainer: {
         flex: 1,
         justifyContent: 'center',
     },
@@ -109,6 +136,11 @@ const styles = StyleSheet.create({
     },
     header: {
         fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000000'
+    },
+    date: {
+        fontSize: 16,
         fontWeight: 'bold',
         color: '#000000'
     },
@@ -129,9 +161,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#000000',
         borderRadius: 5,
-        paddingLeft: 10,  
+        paddingLeft: 10,
     },
-    moodFaces:{
+    moodFaces: {
         marginTop: 10,
         height: 50,
         width: 66,
@@ -155,6 +187,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         textAlign: 'center',
+        paddingTop: 20,
     },
     submitButton: {
         padding: 15,
