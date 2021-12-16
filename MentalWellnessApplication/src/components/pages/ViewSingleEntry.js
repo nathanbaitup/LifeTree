1caf50404933abf24e66c07599ba2780f2f043e2
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-12-16 00:32:03 +0000

    Updated firestore query to get a journal by timestamp and save data to the state to return to the user. Code cleanup.

--- MentalWellnessApplication/src/components/pages/ViewSingleEntry.js
+++ MentalWellnessApplication/src/components/pages/ViewSingleEntry.js
@@ -1,95 +1,95 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, ImageBackground, TextInput, Image, TouchableOpacity } from 'react-native';
 
+// Imports firestore from firebase to display the selected user entry.
 import firestore from '@react-native-firebase/firestore';
 
 export default function ViewSingleEntry(props) {
-    
-    const [allEntries, setAllEntries] = useState([]);
-    const [journalEntry, setJournalEntry] = useState([]);
 
-    
+    // Initializing the state to save firestore fields to their corresponding variables for display.
+    const [selectedMood, setSelectedMood] = useState('');
+    const [journalEntry, setJournalEntry] = useState('');
+    const [displayDate, setDisplayDate] = useState('');
+
+    // Parses the userID from the entries list.
     const userID = props.extraData;
+    // Parses the onBack method to return the user to the entries list when the back button is pressed.
     const onBack = props.onBack;
+    //  Parses the entryID of the selected journal to ensure the correct image is displayed.
     const entryID = props.currentEntryID;
 
+
+    // Creates a reference to the journal list collection in firestore to save data.
     const journalsRef = firestore().collection('journalList');
-    
-    // Selects all journal entries where the user ID matches the authorID and sorts the list by newest date first.
+
+    // Selects the journal entry where the user ID matches the authorID and createdAt timestamp matches the current entry ID
+    // and saves each field to its respectable variable to be displayed to the user when they select an entry to display.
     useEffect(() => {
         journalsRef
             .where('authorID', '==', userID)
-            .orderBy('createdAt', 'desc')
+            .where('createdAt', '==', entryID)
             .onSnapshot(
                 querySnapshot => {
-                    const newJournals = [];
                     querySnapshot.forEach((doc) => {
                         const journal = doc.data();
-                        journal.id = doc.id;
-                        newJournals.push(journal);
+                        setSelectedMood(journal.moodSelected);
+                        setJournalEntry(journal.journalText);
+                        setDisplayDate(journal.dateOfEntry);
                     });
-                    setAllEntries(newJournals);
                 },
                 error => {
                     alert(error);
                 }
             );
-            let selectedEntry = allEntries.find(entry => entry.createdAt === entryID);
-            setJournalEntry(selectedEntry); 
     }, []);
 
+    return (
+        <ImageBackground source={require('../resources/img/background.png')} style={{ width: '100%', height: '100%', opacity: 50 }} >
+            <View style={styles.contentContainer}>
+                <Text style={styles.dateTitle}>Date of Entry: {displayDate} </Text>
+                <Text style={styles.header}>How you were feeling: </Text>
 
-
-
-        return (
-            // TODO: Import updated add an entry page from entries branch to get the mood images to highlight the users mood for an entry.
-            <ImageBackground source={require('../resources/img/background.png')} style={{ width: '100%', height: '100%', opacity: 50 }} >
-                <View style={styles.contentContainer}>
-                    <Text style={styles.dateTitle}>Date of Entry:  {console.log(allEntries)} </Text>
-                    <Text style={styles.header}>How you were feeling: </Text>
-
-                    <View style={styles.moodModules}>
-                        <View style={journalEntry.moodSelected === 'Angry' ? styles.moodModSelected : styles.moodModUnselected} >
-                            <Image source={require('../resources/img/faces/angry.png')} style={styles.moodFaces} />
-                            <Text>Angry</Text>
-                        </View>
-                        <View style={journalEntry.moodSelected === 'Sad' ? styles.moodModSelected : styles.moodModUnselected}>
-                            <Image source={require('../resources/img/faces/sad.png')} style={styles.moodFaces} />
-                            <Text>Sad</Text>
-                        </View>
-                        <View style={journalEntry.moodSelected === 'Meh' ? styles.moodModSelected : styles.moodModUnselected}>
-                            <Image source={require('../resources/img/faces/meh.png')} style={styles.moodFaces} />
-                            <Text>Meh</Text>
-                        </View>
-                        <View style={journalEntry.moodSelected === 'Happy' ? styles.moodModSelected : styles.moodModUnselected}>
-                            <Image source={require('../resources/img/faces/happy.png')} style={styles.moodFaces} />
-                            <Text>Happy</Text>
-                        </View>
+                <View style={styles.moodModules}>
+                    <View style={selectedMood === 'Angry' ? styles.moodModSelected : styles.moodModUnselected} >
+                        <Image source={require('../resources/img/faces/angry.png')} style={styles.moodFaces} />
+                        <Text>Angry</Text>
                     </View>
-
-                    <Text style={[{ textAlign: 'center' }, {color: journalEntry.moodSelected === 'Happy' ? '#108206' : journalEntry.moodSelected === 'Meh' ? '#e38e07' : journalEntry.moodSelected === 'Sad' ? '#112dec' : '#f90505' }]}>
-                       You were feeling: {journalEntry.moodSelected}
-                    </Text>
-                    <View>
-                        <Text style={styles.journalHeader}>Your comments for the day: </Text>
-                        <TextInput style={styles.journalEntry}
-                            placeholder='Feel free to dump as much or as little information in here as you want. We wont judge you at all. We promise! '
-                            numberOfLines={5}
-                            multiline
-                            editable={false}
-                            value={journalEntry.journalText}
-                        />
-                        <View style={styles.returnButtonContainer}>
-                            <TouchableOpacity style={styles.returnButton} onPress={onBack}>
-                                <Text style={styles.returnText}>Return to Entries</Text>
-                            </TouchableOpacity>
-                        </View>
+                    <View style={selectedMood === 'Sad' ? styles.moodModSelected : styles.moodModUnselected}>
+                        <Image source={require('../resources/img/faces/sad.png')} style={styles.moodFaces} />
+                        <Text>Sad</Text>
+                    </View>
+                    <View style={selectedMood === 'Meh' ? styles.moodModSelected : styles.moodModUnselected}>
+                        <Image source={require('../resources/img/faces/meh.png')} style={styles.moodFaces} />
+                        <Text>Meh</Text>
+                    </View>
+                    <View style={selectedMood === 'Happy' ? styles.moodModSelected : styles.moodModUnselected}>
+                        <Image source={require('../resources/img/faces/happy.png')} style={styles.moodFaces} />
+                        <Text>Happy</Text>
                     </View>
                 </View>
-            </ImageBackground>
-        );
-    }
 
+                <Text style={[{ textAlign: 'center' }, { color: selectedMood === 'Happy' ? '#108206' : selectedMood === 'Meh' ? '#e38e07' : selectedMood === 'Sad' ? '#112dec' : '#f90505' }]}>
+                    You were feeling: {selectedMood}
+                </Text>
+                <View>
+                    <Text style={styles.journalHeader}>Your comments for the day: </Text>
+                    <TextInput style={styles.journalEntry}
+                        placeholder='Feel free to dump as much or as little information in here as you want. We wont judge you at all. We promise! '
+                        numberOfLines={5}
+                        multiline
+                        editable={false}
+                        value={journalEntry}
+                    />
+                    <View style={styles.returnButtonContainer}>
+                        <TouchableOpacity style={styles.returnButton} onPress={onBack}>
+                            <Text style={styles.returnText}>Return to Entries</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </ImageBackground>
+    );
+}
 
 // The styling for the page.
 const styles = StyleSheet.create({
@@ -122,9 +122,9 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: '#000000',
         borderRadius: 5,
-        paddingLeft: 10,  
+        paddingLeft: 10,
     },
-    moodFaces:{
+    moodFaces: {
         marginTop: 10,
         height: 50,
         width: 66,
@@ -137,6 +137,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         maxHeight: 250,
         height: 150,
+        color: '#000000',
     },
     journalHeader: {
         fontSize: 18,
@@ -168,5 +169,6 @@ const styles = StyleSheet.create({
         padding: 15,
         fontWeight: 'bold',
         fontSize: 18,
+        color: '#000000',
     }
 });
