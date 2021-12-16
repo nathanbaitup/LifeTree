d407dd3b6ec56535a0694bcb88812afa9d53be14
Nathan Baitup <baitupnw@cardiff.ac.uk>
2021-12-16 02:21:33 +0000

    Code Cleanup

--- MentalWellnessApplication/src/components/pages/Settings.js
+++ MentalWellnessApplication/src/components/pages/Settings.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Text, View, ImageBackground, Button, StyleSheet } from 'react-native';
 
-export default function Settings (props) {
+export default function Settings(props) {
 
     // Parsing the logout function from App.js
     const logout = props.logout;
 
-        return (
-            <ImageBackground source={require('../resources/img/background.png')} style = {{width:'100%', height:'100%', opacity:50}} >
+    return (
+        <ImageBackground source={require('../resources/img/background.png')} style={{ width: '100%', height: '100%', opacity: 50 }} >
             <View style={styles.contentContainer}>
                 <Text> Settings Page </Text>
                 <Button title="Logout" onPress={logout} />
             </View>
-            </ImageBackground>
-        );
-    }
+        </ImageBackground>
+    );
+}
 
-    // The styling for the settings page.
+// The styling for the settings page.
 const styles = StyleSheet.create({
     contentContainer: {
         margin: 20,
