4a0f80d39fa6166b4fc3284704f398b715e90eb3
Nathan Baitup <baitupnw@cardiff.ac.uk>
2022-01-09 20:55:51 +0000

    Added testing to mood journal to ensure the page renders correctly.

--- MentalWellnessApplication/__tests__/MoodJournal/MoodJournal.test.js
+++ MentalWellnessApplication/__tests__/MoodJournal/MoodJournal.test.js
@@ -5,10 +5,28 @@ import 'react-native';
 import React from 'react';
 import MoodJournal from '../../src/components/pages/MoodJournal/MoodJournal';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import { cleanup, render } from '@testing-library/react-native';
 
-it('Checks to see if the Mood Journal page renders', () => {
-  const tree = renderer.create(<MoodJournal />).toJSON();
-  expect(tree).toMatchSnapshot();
+afterEach(cleanup);
+
+describe('Testing that mood journal is rendered', () => {
+  const extraData = { 'id': 1, 'fullName': 'John Doe' };
+  const moodJournalRender = render(<MoodJournal extraData={extraData} />).toJSON();
+
+  it('Checks to see if the Home page renders', () => {
+    expect(moodJournalRender).toMatchSnapshot();
+  });
+
+  it('Checks if the key renders', () => {
+    const {getByTestId} = render(<MoodJournal extraData={extraData} />);
+    const keyText = getByTestId('keyID');
+    expect(keyText.props.children).toEqual('Mood Key:');
+  });
+
+  it('Checks if the calendar renders', () => {
+  const {getByTestId} = render(<MoodJournal extraData={extraData} />);
+  const calendar = getByTestId('calendarID');
+  expect(calendar.type).toEqual('View');
+  });
+
 });
